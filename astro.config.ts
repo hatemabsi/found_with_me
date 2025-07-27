963c06adf99216dfe1360780710732a027f2d9b1
Hatem Absi <hatemalabsi@icloud.com>
2025-07-27 03:30:50 +0100

    fixed dumb mistake

--- astro.config.ts
+++ astro.config.ts
@@ -24,7 +24,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 export default defineConfig({
   output: 'static',
   site: 'https://hatemabsi.github.io',
-  base: '/found-with-me',
+  base: '/found_with_me',
 
   integrations: [
     tailwind({
